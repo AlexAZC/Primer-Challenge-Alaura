2a93c8a53ab801b531506e83b6480efe11473acc
AlexAZC <Alex_Amed_Z_C@outlook.com>
2024-02-04 11:19:26 -0500

    added media queries

---
+++ app.js
@@ -0,0 +1,46 @@
+/*
+const mensaje = "bienvenidos estudiantes";
+
+
+    const splitMensaje = mensaje.split("");
+    let scriptedCode = [];
+
+        for(let i = 0; i < splitMensaje.length; i++){
+            switch(splitMensaje[i]){
+                case "e":
+                    scriptedCode.push("enter");
+                    break;
+                case "i":
+                    scriptedCode.push("imes");
+                    break;
+                case "a":
+                    scriptedCode.push("ai");
+                    break;
+                case "o":
+                    scriptedCode.push("ober");
+                    break;
+                case "u":
+                    scriptedCode.push("ufat");
+                    break;
+                default:
+                    scriptedCode.push(splitMensaje[i]);
+            }
+        }
+
+       let joinedMensaje = scriptedCode.join("");
+        console.log(joinedMensaje);
+
+       let changeE = joinedMensaje.replaceAll(/enter/gi,"e");
+       let changeI = changeE.replaceAll(/imes/gi,"i");
+       let changeA = changeI.replaceAll(/ai/gi,"a");
+       let changeO = changeA.replaceAll(/ober/gi,"o");
+       let changeU = changeO.replaceAll(/ufat/gi,"u");
+
+       console.log(changeU);
+
+        
+
+
+    //console.log(splitMensaje);
+    //console.log(scriptedCode);
+*/
