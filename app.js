ff78250b5d0af9f91d198e2fdbbdce0e5df4f8b4
AlexAZC <Alex_Amed_Z_C@outlook.com>
2024-02-10 22:24:14 -0500

    realizar el boton de copiar

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ let resultado = document.querySelector(".parteDerecha");
 let advertenciaEl = document.querySelector(".advertencia");
 let iconoEl = document.querySelector(".icono");
 
-
-
-console.log(resultado);
-console.log(textareaEl);
+/************** CREANDO LA FUNCIÓN DE ENCRIPTAR  *************/
 
    function encriptarTexto(){
 
@@ -17,81 +14,78 @@ console.log(textareaEl);
        return setTimeout(() => {
             advertenciaEl.style.color = "#495057";
             iconoEl.setAttribute("fill","#495057");
-        }, 2500)
-    }
+        }, 2500);
 
-
+    } else {
         const splitMensaje = textareaEl.value.split("");
         let mensajeLoop = [];
-
+        
         for(let i = 0; i < splitMensaje.length; i++){
             switch(splitMensaje[i]){
                 case "e":
                     mensajeLoop.push("enter");
                     break;
                 case "i":
-                    mensajeLoop.push("imes");
-                    break;
+                        mensajeLoop.push("imes");
+                        break;
                 case "a":
-                    mensajeLoop.push("ai");
-                    break;
+                        mensajeLoop.push("ai");
+                        break;
                 case "o":
-                    mensajeLoop.push("ober");
-                    break;
+                        mensajeLoop.push("ober");
+                        break;
                 case "u":
-                    mensajeLoop.push("ufat");
-                    break;
+                        mensajeLoop.push("ufat");
+                        break;
                 default:
-                    mensajeLoop.push(splitMensaje[i]);
+                        mensajeLoop.push(splitMensaje[i]);
+                                    }
+                                }
+                            
+                let mensajeEncriptado = `<p class="resultadoParrafo">${mensajeLoop.join("")}</p>`;
+                resultado.innerHTML = "";
+                resultado.insertAdjacentHTML("afterbegin",mensajeEncriptado);
+                return;     
             }
-        }
-
-    
-        //let mensajeEncriptado = mensajeLoop.join("");
-
-        let mensajeEncriptado = `<p class="resultadoParrafo">${mensajeLoop.join("")}</p>`;
-        resultado.innerHTML = "";
-        resultado.insertAdjacentHTML("afterbegin",mensajeEncriptado);
-        
-        
    }
+
+
+/************** CREANDO LA FUNCIÓN DE REGEX  *************/
 
    function verificarInput(value) {
     const regex = new RegExp("[^ a-z]");
     return regex.test(value);
-}
-
-/*
-const mensaje = "bienvenidos estudiantes";
-
-
-    const splitMensaje = mensaje.split("");
-    let scriptedCode = [];
-
-        for(let i = 0; i < splitMensaje.length; i++){
-            switch(splitMensaje[i]){
-                case "e":
-                    scriptedCode.push("enter");
-                    break;
-                case "i":
-                    scriptedCode.push("imes");
-                    break;
-                case "a":
-                    scriptedCode.push("ai");
-                    break;
-                case "o":
-                    scriptedCode.push("ober");
-                    break;
-                case "u":
-                    scriptedCode.push("ufat");
-                    break;
-                default:
-                    scriptedCode.push(splitMensaje[i]);
-            }
         }
 
-       let joinedMensaje = scriptedCode.join("");
-        console.log(joinedMensaje);
+
+/************** CREANDO LA FUNCIÓN DE DESENCRIPTAR  *************/
+
+    function desencriptarTexto() {
+        if(verificarInput(textareaEl.value) === true){
+            advertenciaEl.style.color = "red";
+            iconoEl.setAttribute("fill","red");
+        
+            return setTimeout(() => {
+                advertenciaEl.style.color = "#495057";
+                iconoEl.setAttribute("fill","#495057");
+            }, 2500);
+        
+        } else {
+            let changeE = textareaEl.value.replaceAll(/enter/gi,"e");
+            let changeI = changeE.replaceAll(/imes/gi,"i");
+            let changeA = changeI.replaceAll(/ai/gi,"a");
+            let changeO = changeA.replaceAll(/ober/gi,"o");
+            let changeU = changeO.replaceAll(/ufat/gi,"u");
+
+            let mensajeEncriptado = `<p class="resultadoParrafo">${changeU}</p>`;
+                resultado.innerHTML = "";
+                resultado.insertAdjacentHTML("afterbegin",mensajeEncriptado);
+                return;    
+        }
+    }
+
+/*
+
 
        let changeE = joinedMensaje.replaceAll(/enter/gi,"e");
        let changeI = changeE.replaceAll(/imes/gi,"i");
